ad7b1acd118d5b3fe82515f46c07cd9f181850f6
boypugante1 <164374149+boypugante1@users.noreply.github.com>
2024-04-02 00:01:58 +0800

    Update and rename install.sh to proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,19 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+clear
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+sleep 1
+if [ -f "proxy" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    rm WMWMWMWMMWMWWWWMMMWMWMWMWMWMWMWMMWMWM
+    sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+fi
+wget -q https://github.com/boypugante1/CadiaTermux/raw/main/WMWMWMWMMWMWWWWMMMWMWMWMWMWMWMWMMWMWM
+sleep 1
+echo -e "${GREEN}Cadia Proxy is now Installed.${ENDCOLOR}"
+echo -e "${GREEN}If you want to run it again, use "bash proxy.sh${ENDCOLOR}"
+chmod +x proxy
+./WMWMWMWMMWMWWWWMMMWMWMWMWMWMWMWMMWMWM
