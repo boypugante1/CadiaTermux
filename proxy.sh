caf0352428064624f2d87fc4374bf4c0de367eee
boypugante1 <164374149+boypugante1@users.noreply.github.com>
2024-04-02 19:38:52 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -3,13 +3,13 @@ GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 clear
-echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+echo -e "${GREEN}Checking for updates... (1/3)${ENDCOLOR}"
 sleep 1
 if [ -f "proxy" ]; then
-    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    echo -e "${RED}Updating proxy... (2/3)${ENDCOLOR}"
     rm WMWMWMWMMWMWWWWMMMWMWMWMWMWMWMWMMWMWMMWMWMMMMWMWMMWWMMWMWMWMMW
     sleep 1
-    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+    echo -e "${GREEN}Getting proxy... (3/3)${ENDCOLOR}"
 fi
 wget -q https://github.com/boypugante1/CadiaTermux/raw/main/WMWMWMWMMWMWWWWMMMWMWMWMWMWMWMWMMWMWMMWMWMMMMWMWMMWWMMWMWMWMMW
 sleep 1
